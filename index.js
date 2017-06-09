8c9b1b87fb644452a8b18880463c4091bed2a75b
rafer45 <ataque992@hotmail.com>
2017-06-08 21:36:56 -0500

    Add login and ping functionality

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello world!")
+
+const fs = require('fs')
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const token = fs.readFileSync('./api_key.txt', 'utf8');
+console.log(token)
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('pong');
+  }
+});
+
+client.login(token);
