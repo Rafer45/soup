e65d2f440959279503f2254df9a51ba60edf9faf
rafer45 <ataque992@hotmail.com>
2017-06-09 19:02:16 -0500

    Add quit functionality

--- index.js
+++ index.js
@@ -4,16 +4,30 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const token = fs.readFileSync('./api_key.txt', 'utf8');
-console.log(token)
 
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
 client.on('message', message => {
-  if (message.content === 'ping') {
-    message.reply('pong');
+  var msg = message.content.toLowerCase();
+  switch(msg) {
+    case 'ping':
+      message.reply('pong');
+      break;
+    case 'die':
+      client.destroy((err) => {
+        console.log(err);
+      });
+      break;
+    default:
+      break;
   }
+});
+
+bot.on("disconnected", () => {
+  console.log("Disconnected!");
+  process.exit(0);
 });
 
 client.login(token);
