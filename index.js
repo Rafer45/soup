82157d68bd4c6518eb4acd63546d22a5a8100d9d
rafer45 <ataque992@hotmail.com>
2017-06-12 04:35:15 -0500

    Improve echo command

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 
-const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
-const token = fs.readFileSync('./data/api_key.txt', 'utf8');
+const config = require("./config.json");
 
 client.on('ready', () => {
-  console.log('I am ready!');
+  console.log("I am ready!");
 });
 
 client.on('message', (message) => {
-  let prefix = 's.';
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-  let msg = message.content.slice(prefix.length);
+  let msg = message.content.slice(config.prefix.length);
 
   if (msg.startsWith('ping')) {
     message.reply('pong');
+  }
+
+  if (msg.startsWith('echo')) {
+    if (msg.length > 5) {
+      message.channel.send(msg.slice(5));
+    } else {
+      message.channel.send("Please provide valid text to echo!");
+    }
   }
 
   if (msg.startsWith('die')) {
@@ -26,14 +30,11 @@ client.on('message', (message) => {
     });
   }
 
-  if (msg.startsWith('echo')) {
-    message.channel.send(message.content);
-  }
 });
 
-client.on("disconnected", () => {
+client.on('disconnected', () => {
   console.log("Disconnected!");
   process.exit(0);
 });
 
-client.login(token);
+client.login(config.token);
