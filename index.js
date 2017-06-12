909debf2f355ec8fc832b85afb136b40841656d1
rafer45 <ataque992@hotmail.com>
2017-06-12 15:24:07 -0500

    Improve basic command parsing

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
 const config = require("./config.json");
+
+String.prototype.isCommand = function(commandString) {
+  let str = this.valueOf();
+  return str === commandString ||
+         str.startsWith(commandString + ' ');
+};
 
 client.on('ready', () => {
   console.log("I am ready!");
 });
 
 client.on('message', (message) => {
-  if (!message.content.startsWith(config.prefix) || message.author.bot) return; // Ignore messages not
-                                                                                // starting with your prefix
+  // Ignore bot messages or messages not starting with prefix
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
   let msg = message.content.slice(config.prefix.length);
 
-  if (msg.startsWith('ping')) {
+  if (msg.isCommand('ping')) {
     message.reply('pong');
   }
 
-  if (msg.startsWith('echo')) {
-    if (msg.length > 5 && msg[4] === ' ') { // If msg starts with 'echo '
+  if (msg.isCommand('echo')) {
+    if (msg.length > 5) { // If msg is starts with 'echo '
       message.channel.send(msg.slice(5));   // Echo the rest of msg.
     } else {
       message.channel.send("Please provide valid text to echo!");
     }
   }
 
-  if (msg.startsWith('die')) {
+  if (msg.isCommand('die')) {
     message.channel.send("Shutting down...");
     client.destroy((err) => {
       console.log(err);
