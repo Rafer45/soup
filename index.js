763800095215ad93f4fc371f49ca6b9cfa5ebd76
rafer45 <ataque992@hotmail.com>
2017-07-19 15:45:15 -0500

    Lint index.js

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 
 const Discord = require('discord.js');
+const config = require('./config.json');
+const commands = require('./commands.js');
+
 const client = new Discord.Client();
 
-const config = require('./config.json');
-const commands = require('./commands.js')
-
 client.on('ready', () => {
-    console.log("I am ready!");
+    console.log('I am ready!');
     client.owner = client.users.get(config.ids.soupmaster);
-    
-    let spam = client.channels.get(config.ids.spam_channel);
+    const spam = client.channels.get(config.ids.spam_channel);
 
     spam.send(`${client.owner}, soup is ready!`);
 });
@@ -18,19 +17,19 @@ client.on('message', (message) => {
   // Ignore bot messages or messages not starting with prefix
     if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-    let _content = message.content.slice(config.prefix.length),
-        [command, ...params] = _content.split(/ +/),
-        msg      = _content.slice(command.length + 1);
+    const contents = message.content.slice(config.prefix.length);
+    const [command, ...params] = contents.split(/ +/);
+    const msg = contents.slice(command.length + 1);
 
     if (commands[command] !== undefined) {
         commands[command](message, config, msg, ...params);
     } else {
-        message.reply("that's not a valid command.");
+        message.reply('that\'s not a valid command.');
     }
 });
 
 client.on('disconnected', () => {
-    console.log("Disconnected!");
+    console.log('Disconnected!');
     process.exit(0);
 });
 
