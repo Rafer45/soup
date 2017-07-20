1c4d428455a69c48ea5407b8a22a3319184b1735
rafer45 <ataque992@hotmail.com>
2017-07-19 20:08:58 -0500

    Fix addCommands, improve music commands

--- commands.js
+++ commands.js
@@ -1,6 +1,4 @@
 
-// const fs = require('fs');
-// const musicCommand = require('./commands/music.js');
 const {
     musicCommands,
     randomnessCommands,
@@ -9,20 +7,8 @@ const {
     testingCommands,
 } = require('./commands/');
 
-// console.log(musicCommands);
-
-// const musicCommands = require('./commands/music_commands');
-// const randomnessCommands = require('./commands/randomness_commands');
-// const sillyCommands = require('./commands/silly_commands');
-// const soupmasterCommands = require('./commands/soupmaster_commands');
-// const testingCommands = require('./commands/testing_commands');
-
-// Commands are called in the following manner:
-// commands[command](message, config, msg, ...parameters)
-// msg is the message content without the prefix or command
 module.exports = {};
 
-/* eslint dot-notation: "warn" */
 function addCommands(...objs) {
     objs.forEach((obj) => {
         const fn = obj.fn || (x => x);
--- commands/music_commands.js
+++ commands/music_commands.js
@@ -5,13 +5,24 @@ const ytdl = require('ytdl-core');
 // Will play a song to its conclusion with no way to stop it.
 // Just a bit of an exercise for now.
 module.exports = {
-    braindead: (message, config, msg, url) => {
+    braindead: (message, config, _, url) => {
         const vc = message.member.voiceChannel;
         if (!vc) {
             return message.reply('Join a voice channel before using the command.');
         }
         if (!url) {
             return message.reply('Provide a valid URL.');
+        }
+
+        if (message.guild.voiceConnection) {
+            return message.channel.send('Interrupting song...')
+                .then(() => {
+                    const dispatcher = message.guild.voiceConnection.dispatcher;
+                    dispatcher.end();
+                    setTimeout(() => {
+                        module.exports.braindead(message, config, _, url);
+                    }, 500);
+                });
         }
         // Adapted from https://github.com/fent/node-ytdl-core/blob/master/example/discord.js
         return vc.join()
@@ -21,13 +32,25 @@ module.exports = {
                 });
                 const dispatcher = c.playStream(
                     stream,
-                    { passes: config.passes },
+                    { passes: config.music.passes },
                 );
+                dispatcher.setVolume(config.music.volume);
                 dispatcher.on('end', () => {
                     vc.leave();
                 });
                 message.channel.send(`Playing song at url:\n${url}`);
             })
             .catch(console.error);
+    },
+
+    pleasestop: (message) => {
+        if (message.guild.voiceConnection) {
+            return message.channel.send('Interrupting song...')
+                .then(() => {
+                    const dispatcher = message.guild.voiceConnection.dispatcher;
+                    dispatcher.end();
+                });
+        }
+        return message.channel.send('Stop what?');
     },
 };
