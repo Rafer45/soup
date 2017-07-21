7ebb03b17e4b95fc2fb9ed699db306cfb2c2bf41
rafer45 <ataque992@hotmail.com>
2017-07-20 23:22:26 -0500

    Ugly (but functional) queue command

--- commands/music_commands.js
+++ commands/music_commands.js
@@ -1,28 +1,31 @@
 
 const ytdl = require('ytdl-core');
 
-// Most braindead play command possible.
-// Will play a song to its conclusion with no way to stop it.
-// Just a bit of an exercise for now.
+let queue = {};
+
 module.exports = {
-    braindead: (message, config, _, url) => {
+    fn: command => (
+        (message, ...other) => {
+            command(message.guild.voiceConnection, message, ...other);
+        }
+    ),
+
+    violence: (voiceConn, message, config, _, url) => {
+        if (voiceConn) {
+            return module.exports.pleasestop(voiceConn, message)
+                .then(() => {
+                    setTimeout(() => {
+                        module.exports.violence(voiceConn, message, config, _, url);
+                    }, 500);
+                });
+        }
+
         const vc = message.member.voiceChannel;
         if (!vc) {
             return message.reply('Join a voice channel before using the command.');
         }
         if (!url) {
             return message.reply('Provide a valid URL.');
-        }
-
-        if (message.guild.voiceConnection) {
-            return message.channel.send('Interrupting song...')
-                .then(() => {
-                    const dispatcher = message.guild.voiceConnection.dispatcher;
-                    dispatcher.end();
-                    setTimeout(() => {
-                        module.exports.braindead(message, config, _, url);
-                    }, 500);
-                });
         }
         // Adapted from https://github.com/fent/node-ytdl-core/blob/master/example/discord.js
         return vc.join()
@@ -36,21 +39,53 @@ module.exports = {
                 );
                 dispatcher.setVolume(config.music.volume);
                 dispatcher.on('end', () => {
-                    vc.leave();
+                    if (queue[message.guild.id] && queue[message.guild.id].length > 0) {
+                        const newStream = ytdl(queue[message.guild.id].shift(), {
+                            filter: 'audioonly',
+                        });
+                        c.playStream(
+                            newStream,
+                            { passes: config.music.passes },
+                        ).setVolume(config.music.volume);
+                    } else {
+                        vc.leave();
+                    }
                 });
                 message.channel.send(`Playing song at url:\n${url}`);
             })
             .catch(console.error);
     },
 
-    pleasestop: (message) => {
-        if (message.guild.voiceConnection) {
-            return message.channel.send('Interrupting song...')
-                .then(() => {
-                    const dispatcher = message.guild.voiceConnection.dispatcher;
-                    dispatcher.end();
-                });
+    pleasestop: (voiceConn, message) => {
+        if (voiceConn) {
+            return new Promise((resolve, reject) => {
+                message.channel.send('Interrupting song...')
+                    .then((promiseMessage) => {
+                        const dispatcher = voiceConn.dispatcher;
+                        dispatcher.end();
+                        resolve(promiseMessage);
+                    })
+                    .catch(reject);
+            });
         }
         return message.channel.send('Stop what?');
+    },
+
+    play: (voiceConn, message, _, config, url) => {
+        if (queue[message.guild.id]) {
+            queue[message.guild.id].push(url);
+        } else {
+            queue[message.guild.id] = [url];
+        }
+        if (!voiceConn) {
+            module.exports.violence(voiceConn, message, _, config, queue[message.guild.id].shift);
+        }
+        return message.channel.send(`Added this song to the queue:\n${url}`);
+            // .then(() => {
+            //     const dispatcher = voiceConn.dispatcher;
+            //     dispatcher.on('end', () => {
+            //         module.exports.violence(voiceConn, message, config, _, url);
+            //     });
+            // });
     },
 };
