85dd041b3336e7ededcfd61e8cbf83c6dd01b91b
rafer45 <ataque992@hotmail.com>
2017-07-28 15:28:20 -0500

    Create full new music command system

--- commands/music_commands.js
+++ commands/music_commands.js
@@ -4,81 +4,65 @@ const ytdl = require('ytdl-core');
 const queues = {};
 
 module.exports = {
-    fn: command => (
-        (message, ...other) => {
-            command(message.guild.voiceConnection, message, ...other);
-        }
-    ),
+    play: (message, config, _, url) => {
+        module.exports.enqueue(message, null, null, url);
+        const voiceConn = message.guild.voiceConnection;
 
-    braindead: (voiceConn, message, config, _, url) => {
         if (voiceConn) {
-            return module.exports.pleasestop(voiceConn, message)
-                .then(() => {
-                    setTimeout(() => {
-                        module.exports.braindead(null, message, config, _, url);
-                    }, 500);
-                })
-                .catch(console.error);
+            return false;
         }
 
         const vc = message.member.voiceChannel;
+
         if (!vc) {
             return message.reply('Join a voice channel before using the command.');
         }
-        if (!url) {
-            if (queues[message.guild.id].length > 0) {
-                return module.exports.braindead(
+
+        if (queues[message.guild.id].length === 0) {
+            return message.reply(`No songs in queue. Add songs with \`${config.prefix}enqueue\` or provide a url.`);
+        }
+        return vc.join()
+            .then(dispatch.bind(
                     null,
                     message,
-                    config,
-                    _,
                     queues[message.guild.id].shift(),
-                );
-            }
-            return message.reply('Provide a valid URL or add something to the queue.');
-        }
-        // Adapted from https://github.com/fent/node-ytdl-core/blob/master/example/discord.js
-        return vc.join()
-            .then((c) => {
-                const stream = ytdl(url, {
-                    filter: 'audioonly',
-                });
-                const dispatcher = c.playStream(
-                    stream,
-                    { passes: config.music.passes },
-                );
-                dispatcher.setVolume(config.music.volume);
-                dispatcher.on('end', () => {
-                    if (queues[message.guild.id].length > 0) {
-                        setTimeout(() => {
-                            module.exports.braindead(
-                                null,
-                                message,
-                                config,
-                                _,
-                                queues[message.guild.id].shift(),
-                            );
-                        }, 500);
-                    } else {
-                        vc.leave();
-                    }
-                });
-                message.channel.send(`Playing song at url:\n${url}`);
-            })
-            .catch((error) => {
-                console.error(error);
-                message.channel.send('Something went wrong. Either the URL is invalid, or soup is having issues.');
+                    config,
+                ));
+    },
+    // enqueue is a mess and I'll fix it later
+    enqueue: (message, _, __, url) => {
+        if (url) {
+            ytdl.getInfo(url, (e) => {
+                if (e) {
+                    url = undefined;
+                    message.channel.send('Invalid YouTube URL.');
+                }
             });
+        }
+
+        if (queues[message.guild.id]) {
+            if (url) {
+                queues[message.guild.id].push(url);
+            }
+        } else {
+            queues[message.guild.id] = (url ? [url] : []);
+        }
+
+        if (url) {
+            message.channel.send(`Enqueued song at url ${url}.`);
+        }
+        return console.log(queues);
     },
 
-    stop: (voiceConn, message) => {
+    stop: (message) => {
+        const voiceConn = message.guild.voiceConnection;
         if (voiceConn) {
             return new Promise((resolve, reject) => {
                 message.channel.send('Interrupting song...')
-                    .then((promiseMessage) => {
+                    .then((_message) => {
                         const dispatcher = voiceConn.dispatcher;
                         dispatcher.end();
-                        resolve(promiseMessage);
+                        resolve(_message);
                     })
                     .catch(reject);
             });
@@ -86,26 +70,45 @@ module.exports = {
         return message.channel.send('Stop what?');
     },
 
-    pleasestop: (voiceConn, message) => {
-        message.channel.send('Clearing queue...').then(() => {
-            queues[message.guild.id] = [];
-            module.exports.stop(voiceConn, message);
-        });
+    pleasestop: (message) => {
+        module.exports.clearqueue(message)
+            .then(module.exports.stop);
     },
 
-    enqueue: (voiceConn, message, config, _, url) => {
-        if (queues[message.guild.id]) {
-            queues[message.guild.id].push(url);
-        } else {
-            queues[message.guild.id] = [url];
-        }
-        message.channel.send(`Enqueued song at url ${url}.`);
-        console.log(queues);
-    },
-
-    // time: (voiceConn, message) => {
-    //     if (voiceConn) {
-    //
-    //     }
-    // }
+    clearqueue: message => (
+        new Promise((resolve, reject) => {
+            message.channel.send('Queue cleared.').then((_message) => {
+                queues[message.guild.id] = [];
+                resolve(_message);
+            }).catch(reject);
+        })
+    ),
 };
+
+function dispatch(message, url, config, connection) {
+    const stream = ytdl(url, {
+        filter: 'audioonly',
+    });
+    const dispatcher = connection.playStream(
+        stream,
+        { passes: config.music.passes },
+    );
+    dispatcher.setVolume(config.music.volume);
+    dispatcher.on('end', () => {
+        if (queues[message.guild.id].length > 0) {
+            setTimeout(() => {
+                dispatch(
+                        message,
+                        queues[message.guild.id].shift(),
+                        config,
+                        connection,
+                    );
+            }, 500);
+        } else {
+            message.channel.send('Queue is now empty.').then(() => {
+                connection.channel.leave();
+            });
+        }
+    });
+    message.channel.send(`Playing song at url:\n${url}`);
+}
