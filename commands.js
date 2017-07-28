b315bcc40a6a78228a5a2b3b6b8a6b64bbc06f05
rafer45 <ataque992@hotmail.com>
2017-07-28 18:14:39 -0500

    Improve command importing

--- commands.js
+++ commands.js
@@ -1,27 +1,28 @@
 
-const {
-    musicCommands,
-    randomnessCommands,
-    sillyCommands,
-    soupmasterCommands,
-    testingCommands,
-} = require('./commands/');
+const fs = require('fs');
+
+const commandGroups = [];
+
+fs.readdirSync('./commands')
+    .filter(str => str.endsWith('.js'))
+    .map(str => str.slice(0, -3))
+    .forEach((filename) => {
+        console.log(filename);
+        commandGroups.push(require(`./commands/${filename}`));
+    });
 
 module.exports = {};
+
+addCommands(...commandGroups);
+
 
 function addCommands(...objs) {
     objs.forEach((obj) => {
         const fn = obj.fn || (x => x);
-        Object.keys(obj).filter(x => x !== 'fn').forEach((k) => {
-            module.exports[k] = fn(obj[k]);
-        });
+        Object.keys(obj)
+            .filter(x => x !== 'fn')
+            .forEach((k) => {
+                module.exports[k] = fn(obj[k]);
+            });
     });
 }
-
-addCommands(
-    musicCommands,
-    randomnessCommands,
-    soupmasterCommands,
-    sillyCommands,
-    testingCommands,
-);
