226e934533f6a3566454f04258f4df70b2ca8ba2
rafer45 <ataque992@hotmail.com>
2017-07-28 18:30:53 -0500

    Handle empty messages on echo

--- commands/testing_commands.js
+++ commands/testing_commands.js
@@ -7,6 +7,8 @@ module.exports = {
     echo: (message, _, msg) => {
         if (msg) {
             message.channel.send(msg);
+        } else {
+            message.channel.send('Provide text to echo.');
         }
     },
 };
