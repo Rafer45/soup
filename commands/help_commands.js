b6e11d2c1e05b0a1e057a6c8ce5dd1402ea0eb68
rafer45 <ataque992@hotmail.com>
2017-07-30 16:25:36 -0500

    Outline optional arguments with square brackets and change order again

--- commands/help_commands.js
+++ commands/help_commands.js
@@ -7,12 +7,12 @@ module.exports = {
 ++ MUSIC COMMANDS ++
 ${p}play URL - Plays a song at a YouTube URL.
 ${p}stop     - Interrupts the current song.
-${p}queue    - Says the current queue.
-${p}clearqueue - Clears the queue.
-${p}pleasestop - Clears the queue and interrupts the current song.
 ${p}togglepause - Pauses (or resumes) the current stream.
+${p}queue       - Says the current queue.
+${p}clearqueue  - Clears the queue.
 ${p}volume      - Displays the volume.
 ${p}volumeshift n - Shifts the volume by n%. (Negative numbers can be used to decrease volume).
+${p}pleasestop    - Clears the queue and interrupts the current song.
 
 ++ TESTING COMMANDS ++
 ${p}ping - Says "pong."
@@ -20,8 +20,8 @@ ${p}echo text - Repeats whatever text is given.
 
 ++ RANDOMNESS COMMANDS ++
 ${p}coin - Says heads or tails.
-${p}dice n - Says a random number between 1 and n. 6 is the default.
-${p}factcheck fact - Uses advanced neural networks and quantum bubble extrapolation to calculate a fact's veracity. If no fact is provided, it will give a more general statement about the command user.
+${p}dice [n] - Says a random number between 1 and n. 6 is the default.
+${p}factcheck [fact] - Uses advanced neural networks and quantum bubble extrapolation to calculate a fact's veracity. If no fact is provided, it will give a more general statement about the command user.
 
 ++ MISC. COMMANDS ++
 ${p}effify text - Effifies some text.
